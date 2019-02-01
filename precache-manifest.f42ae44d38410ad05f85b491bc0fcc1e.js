92080b842a40c3fde5b63e1907f109ac4bd0f9ab
Kayla MacDonald <kayla.macdonald@fostermoore.com>
2019-02-01 14:12:09 -0500

    Updates

---
+++ precache-manifest.f42ae44d38410ad05f85b491bc0fcc1e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "13611771ac8616e6db74",
+    "url": "/react-youtube-simple/static/js/runtime~main.13611771.js"
+  },
+  {
+    "revision": "3c35c056dedd96a8bc1d",
+    "url": "/react-youtube-simple/static/js/main.3c35c056.chunk.js"
+  },
+  {
+    "revision": "26387354318e7d868eeb",
+    "url": "/react-youtube-simple/static/js/1.26387354.chunk.js"
+  },
+  {
+    "revision": "beff2eb0f6ea41d379c0d7d67fd82456",
+    "url": "/react-youtube-simple/index.html"
+  }
+];
